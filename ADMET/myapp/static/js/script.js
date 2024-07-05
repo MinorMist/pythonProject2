a4281085b643b4f710b52425a03a516c71a88529
Achyut <48128719+MinorMist@users.noreply.github.com>
2024-07-05 06:35:57 +0530

    tool tip and new updates

--- ADMET/myapp/static/js/script.js
+++ ADMET/myapp/static/js/script.js
@@ -1,23 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 700) {
-            document.getElementById('navbar_top').classList.add('fixed-top');
-            // add padding top to show content behind navbar
-            navbar_height = document.querySelector('.navbar').offsetHeight;
-            document.body.style.paddingTop = navbar_height + 'px';
-        } else {
-            document.getElementById('navbar_top').classList.remove('fixed-top');
-            // remove padding top from body
-            document.body.style.paddingTop = '0';
-        }
-    });
-});
-
 function fill_sample_smile() {
-    document.getElementById("smilesTextarea").value = 'CC(CCC(=O)N)CN\nC1=CC=CC=C1';
+    // Sample smiles array
+    let arr = ["CC(=O)NCCC1=CNc2c1cc(OC)cc2", "CCc(c1)ccc2[n+]1ccc3c2[nH]c4c3cccc4", "CC(C)CC1=CC=C(C=C1)C(C)C(=O)O", "CC1=CN=C(C(=C1OC)C)CS(=O)C2=NC3=C(N2)C=CC(=C3)OC", "CCC[C@@H](O)CC/C=C/C=C/C#CC#C/C=C/CO"];
+    document.getElementById("smilesTextarea").value = arr[(Math.floor(Math.random() * arr.length))];
 };
 
-window.smoothScroll = function(target) {
+window.smoothScroll = function (target) {
     var scrollContainer = target;
     do { //find scroll container
         scrollContainer = scrollContainer.parentNode;
@@ -31,10 +18,10 @@ window.smoothScroll = function(target) {
         targetY += target.offsetTop;
     } while (target = target.offsetParent);
 
-    scroll = function(c, a, b, i) {
+    scroll = function (c, a, b, i) {
         i++; if (i > 30) return;
         c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 7);
+        setTimeout(function () { scroll(c, a, b, i); }, 7);
     }
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
@@ -46,4 +33,6 @@ function copyToClipboard(element) {
     $temp.val($(element).text()).select();
     document.execCommand("copy");
     $temp.remove();
+    var tooltip = document.getElementById("myTooltip1");
+    tooltip.innerHTML = "Copied to clipboard ";
 };
